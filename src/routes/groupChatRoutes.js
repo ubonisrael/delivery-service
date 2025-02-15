077db9949d9bdef3f299885505d9b7d0c1900161
ubonisrael <akpanudohubonisrael@gmail.com>
2025-02-15 07:53:13 +0100

    removed unnecessary files

--- src/routes/groupChatRoutes.js
+++
@@ -1,8 +0,0 @@
-import GroupChatController from "../controllers/groupChatController";
-
-export default function setGroupChatRoutes(app) {
-    const controller = new GroupChatController();
-
-    app.post('/group-chat/send', (req, res) => controller.sendMessage(req, res));
-    app.get('/group-chat/messages', (req, res) => controller.getMessages(req, res));
-};
--- src/routes/privateChatRoutes.js
+++
@@ -1,8 +0,0 @@
-import PrivateChatController from "../controllers/privateChatController";
-
-export default function setPrivateChatRoutes (app) {
-    const privateChatController = new PrivateChatController();
-
-    app.post('/private-chat/send', privateChatController.sendPrivateMessage);
-    app.get('/private-chat/messages/:manufacturerId/:wholesalerId', privateChatController.getPrivateMessages);
-};
