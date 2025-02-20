eb1ef5c89d0fd1ba7c37c92c4052d4940bac42ce
ubonisrael <akpanudohubonisrael@gmail.com>
2025-02-20 01:40:14 +0100

    modify redisClient to make use of env vars

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,5 +1,10 @@
 import Redis from "ioredis";
 
-const redis = new Redis(); // Creates a single Redis connection
-
+const redis = new Redis({
+    host: process.env.REDIS_HOST || "localhost",
+    port: process.env.REDIS_PORT || 6379,
+    username: process.env.REDIS_USERNAME || undefined,
+    password: process.env.REDIS_PASSWD || undefined,
+    retryStrategy: (times) => (times > 10 ? null : Math.min(times * 50, 2000)),
+  });
 export default redis;
