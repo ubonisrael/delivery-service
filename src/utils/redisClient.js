2c004cf764122fe52e231298df2694b96c8b6936
ubonisrael <akpanudohubonisrael@gmail.com>
2025-02-20 15:57:19 +0100

    comment out redisclient file

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,10 +1,10 @@
-import Redis from "ioredis";
+// import Redis from "ioredis";
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || "localhost",
-    port: process.env.REDIS_PORT || 6379,
-    username: process.env.REDIS_USERNAME || undefined,
-    password: process.env.REDIS_PASSWD || undefined,
-    retryStrategy: (times) => (times > 10 ? null : Math.min(times * 50, 2000)),
-  });
-export default redis;
+// const redis = new Redis({
+//     host: process.env.REDIS_HOST || "localhost",
+//     port: process.env.REDIS_PORT || 6379,
+//     username: process.env.REDIS_USERNAME || undefined,
+//     password: process.env.REDIS_PASSWD || undefined,
+//     retryStrategy: (times) => (times > 10 ? null : Math.min(times * 50, 2000)),
+//   });
+// export default redis;
